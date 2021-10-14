a2fc360fb9433b5754dd42a31671c3e9a9f83f61
Brendan Degryse <bdegryse@software.private>
2021-10-14 13:10:36 -0400

    Added printing when a phone number doesn't match

--- src/phonenumbervalidator/PhoneNumberValidator.java
+++ src/phonenumbervalidator/PhoneNumberValidator.java
@@ -17,22 +17,30 @@ public class PhoneNumberValidator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[] phoneNumbers = {"(419)123-4567", "(419) 123-4567", "419-123-4567", "419.123.4567", "419_123_4567", "(419)123-456", "4191234567", "419123456"};//list of phone numbers
-        Pattern expression = Pattern.compile("[(]([0-9]{3})[)]([0-9]{3})-([0-9]{4})|[(]([0-9]{3})[)] ([0-9]{3})-([0-9]{4})|([0-9]{3})-([0-9]{3})-([0-9]{4})|([0-9]{3})[.]([0-9]{3})[.]([0-9]{4})");//pattern
-        Matcher myMatcher;
-        
-        //loop through the numbers and test them
-        for (String phoneNumber : phoneNumbers) {
-            myMatcher = expression.matcher(phoneNumber);
-            if (myMatcher.matches()) {
-                int offset = 1;
-                //find the first number
-                while (myMatcher.group(offset) == null) {
-                    offset++;
+        if (args.length > 0) {
+            String[] phoneNumbers = {"(419)123-4567", "(419) 123-4567", "419-123-4567", "419.123.4567", "419_123_4567", "(419)123-456", "4191234567", "419123456"};//list of phone numbers
+            Pattern expression = Pattern.compile("[(]([0-9]{3})[)]([0-9]{3})-([0-9]{4})|[(]([0-9]{3})[)] ([0-9]{3})-([0-9]{4})|([0-9]{3})-([0-9]{3})-([0-9]{4})|([0-9]{3})[.]([0-9]{3})[.]([0-9]{4})");//pattern
+            Matcher myMatcher;
+
+            //loop through the numbers and test them
+            for (String phoneNumber : phoneNumbers) {
+                myMatcher = expression.matcher(phoneNumber);
+                if (myMatcher.matches()) {
+                    int offset = 1;
+                    //find the first number
+                    while (myMatcher.group(offset) == null) {
+                        offset++;
+                    }
+                    //print the number in the requested format
+                    System.out.println("(" + myMatcher.group(offset) + ") " + myMatcher.group(1 + offset) + "-" + myMatcher.group(2 + offset));
                 }
-                //print the number in the requested format
-                System.out.println("(" + myMatcher.group(offset) + ") " + myMatcher.group(1 + offset) + "-" + myMatcher.group(2 + offset));
+                else {
+                    System.out.println(phoneNumber + " is not a valid phonenumber.");
+                }
             }
+        }
+        else {
+            System.out.println("No arguments given.");
         }
     }
     
